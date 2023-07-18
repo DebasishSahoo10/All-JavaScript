402472993bc3869dad40974b4fe5f3ab2b178268
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-07-19 00:39:33 +0530

    added some more question

---
+++ Declaration/Output.js
@@ -0,0 +1,19 @@
+const testFunc = () => {
+  for (var i = 0; i < 3; i++) {
+    setTimeout(() => {
+      console.log(i);
+    }, i * 1000);
+  }
+};
+testFunc(); // 3,3,3
+// Why : Var is function scoped and setTimeout is an asynchronous code. so setTimeout will only run after the for loop has completely finished. and when for loop is completely finished the value of i will be 3, so set time out will take that value all the three times.
+// But what if the VAR is replaced with LET.
+const testFunc2 = () => {
+  for (let i = 0; i < 3; i++) {
+    setTimeout(() => {
+      console.log(i);
+    }, i * 1000);
+  }
+};
+testFunc2(); // 0,1,2
+// Let is block scoped, so for every loop iteration a new (i) has been declared, so every setTimeout will have reference to a different declaration
