cdace0f037ecb888da3089ee96b25264212ee953
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-07-24 15:36:17 +0530

    Call - Bind - Apply

---
+++ Call - Apply - Bind/Concept.md
@@ -0,0 +1,7 @@
+# Call - Apply - Bind
+
+1. Implicit Binding vs. Explicit Binding vs. Windows Binding
+
+(a) Implicit Binding means accessing a method of an object on that object only. It means the THIS keyword in this scenario will point to the method carrying object here.
+(b) Explicit Binding means accessing values of an object through an external function through Function Borrowing ways like Call - Apply - Bind. Here THIS keyword will refer to the object we are passing as argument to these methods.
+(c) and Window Binding means attaching a function to the global scope and running it on the properties of window object.
---
+++ Call - Apply - Bind/Outputs.js
@@ -0,0 +1,36 @@
+// 1 : Call takes all the argument one by one and Apply takes all the arguments at once. So how to make call take the arguments in an array just like apply
+function func(...langs) {
+  console.log(this.name + " And i Know " + langs);
+}
+const me = {
+  name: "D",
+};
+const languages = ["JS", "PYT"];
+func.call(me, languages); // D And I Know JS,PYT
+// Answer : is Rest Parameters which takes the whole array and give it to the function one by one. but here if the func is an arrow function, it won't work.
+
+// 2 : can we explicitly bind an object to its own methods?
+const testObj = {
+  name: "C",
+  getName() {
+    console.log(this.name);
+  },
+};
+testObj.getName.call(testObj); // C
+// Answer : is Yes. Here also if getName is an arrow function it won't work.
+
+// 3 : as we have seen Explicit Binding methods changes the THIS keyword reference of a function
+const obj = {
+  name: "John",
+  greet: function () {
+    const arrowFunc = () => {
+      console.log(this.name);
+    };
+    arrowFunc();
+  },
+};
+const otherObj = {
+  name: "Jane",
+};
+obj.greet.call(otherObj); // Jane
+// Why : here arrowFunc doesn't have its own THIS, hence it is taking the THIS reference from the parent function declaration. but if we call that function using call, then THIS reference will be changed.
